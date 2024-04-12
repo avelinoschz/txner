df5c900d606438ba0545c59ee6b16e62f3426197
Avelino Sanchez <avelino.schz@gmail.com>
2024-04-12 01:26:47 -0600

    sql(migration): update initial seeder

--- sql/migrations/20240410005522_initial_tables.sql
+++ sql/migrations/20240410005522_initial_tables.sql
@@ -1,36 +1,58 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE card_types (
-    card_type_id INT PRIMARY KEY,
-    card_type_name VARCHAR(20)
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO card_types (card_type_id, card_type_name) VALUES
+ALTER SEQUENCE users_id_seq RESTART WITH 1000;
+
+INSERT INTO users (id, email) VALUES
+(1000, 'john.doe@mail.com');
+
+CREATE TABLE account_types (
+    id BIGSERIAL PRIMARY KEY,
+    short_name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO account_types (id, short_name) VALUES
 (1, 'debit'),
 (2, 'credit');
 
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    email VARCHAR(100)
+CREATE TABLE accounts (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    account_type_id BIGINT NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL CHECK (balance >= 0),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (account_type_id) REFERENCES account_types(id)
 );
 
-CREATE TABLE movements (
-    movement_id INT PRIMARY KEY,
-    user_id INT,
-    movement_date DATE,
-    amount DECIMAL(10, 2),
-    card_type_id INT,
-    description TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (card_type_id) REFERENCES card_types(card_type_id)
+INSERT INTO accounts (id, user_id, account_type_id, balance) VALUES
+(1, 1000, 1, 0),
+(2, 1000, 2, 0);
+
+CREATE TABLE transactions (
+    id BIGINT PRIMARY KEY,
+    account_id BIGINT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    date DATE NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
+
+CREATE INDEX idx_account_transactions ON transactions (account_id, date DESC);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE movements;
+DROP TABLE transactions;
+
+DROP TABLE accounts;
+
+DROP TABLE account_types;
 
 DROP TABLE users;
-
-DROP TABLE card_types;
 -- +goose StatementEnd
